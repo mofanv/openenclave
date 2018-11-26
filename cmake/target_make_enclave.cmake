4f74e82e0cb3052bb28b896fcb17abc1b365d55d
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-11-26 13:30:45 -0800

    Add signed enclave as imported library
    So that it can be referenced via the TARGET_FILE generator expression
    elsewhere. We still need a custom target in order to cause the custom
    commands to be run. This can be cleaned up later.

--- cmake/target_make_enclave.cmake
+++ cmake/target_make_enclave.cmake
@@ -1,41 +1,50 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 #
-# Helper function to create enclave binary
-#
-# Generates a target for an enclave binary
+# Helper function to sign an enclave binary.
 #
 # Usage:
 #
-#	target_make_enclave(
-#		<name> <signconffile> <signkeyfile>
-#		source1 [source2...]
+#	target_make_enclave(<target> <signconffile>)
 #
-# Target properties can be set on <name>, see add_executable for details.
+# The target must already exist and should be a shared object (an
+# enclave).
 #
-# Restrictions: A number of subtleties are not handled, such as
-# - RUNTIME_OUTPUT_DIRECTORY property is not handled correctly
-# - the resulting binary name is not reflected by the target
-#   (complicating install rules)
+# Given <target> and <signconffile>, this function adds custom
+# commands to generate a signing key and call `oesign` to sign the
+# target, resulting in `<target>.signed.so`. It also adds
+# `<target>_signed` as an imported target so that it can be referenced
+# later in the CMake graph.
 #
+# TODO: (1) Rename this to `target_sign_enclave` since it does not
+# make an enclave.
+# TODO: (2) Use `cmake_parse_arguments` to fix the argument names.
+# TODO: (3) Add an optional argument to accept a given signing key
+# instead of generating one.
+# TODO: (4) Replace the name guessing logic.
+# TODO: (5) Setup the dependency using `${BIN}_signed` instead of the
+# default custom target.
 function(target_make_enclave BIN SIGNCONF)
-#set_property(TARGET ${BIN} PROPERTY
-#    PREFIX "")
-#set_property(TARGET ${BIN} PROPERTY SUFFIX "")
-	# custom rule to generate signing key
-	add_custom_command(OUTPUT ${BIN}-private.pem
-		COMMAND openssl genrsa -out ${BIN}-private.pem -3 3072
-		)
+  # Generate the signing key.
+  add_custom_command(OUTPUT ${BIN}-private.pem
+    COMMAND openssl genrsa -out ${BIN}-private.pem -3 3072)
 
-	# custom rule to sign the binary
-	add_custom_command(OUTPUT ${BIN}.signed.so
-		COMMAND oesign $<TARGET_FILE:${BIN}> ${SIGNCONF} ${CMAKE_CURRENT_BINARY_DIR}/${BIN}-private.pem
-		DEPENDS oesign ${BIN} ${SIGNCONF} ${BIN}-private.pem
-		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-		)
+  # TODO: Get this name intelligently (somehow use $<TARGET_FILE> with
+  # `.signed` injected).
+  set(SIGNED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/lib${BIN}.signed.so)
 
-	# signed binary is a default target
-	add_custom_target(${BIN}-signed ALL
-		DEPENDS ${BIN}.signed.so
-		)
-endfunction(target_make_enclave)
+  # Sign the enclave using `oesign`.
+  add_custom_command(OUTPUT ${SIGNED_LOCATION}
+    COMMAND oesign $<TARGET_FILE:${BIN}> ${SIGNCONF} ${CMAKE_CURRENT_BINARY_DIR}/${BIN}-private.pem
+    DEPENDS oesign ${BIN} ${SIGNCONF} ${BIN}-private.pem
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+
+  # Import the generated signed enclave so we can reference it with
+  # `$<TARGET_FILE>` later.
+  add_library(${BIN}_signed SHARED IMPORTED GLOBAL)
+  set_target_properties(${BIN}_signed PROPERTIES
+    IMPORTED_LOCATION ${SIGNED_LOCATION})
+
+  # Add a custom target with `ALL` semantics so these targets are always built.
+  add_custom_target(${BIN}_signed_target ALL DEPENDS ${SIGNED_LOCATION})
+endfunction()
